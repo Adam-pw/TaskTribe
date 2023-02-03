5d813b73668eb3680f8f4805a671cc194537b631
Janmejay Joshi <publish.janmejay@gmail.com>
2023-02-03 21:01:06 +0530

    Added Firebase and Prettier

---
+++ src/utils/firebase/index.ts
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDUtwgCBc5Qu8Ur62hNO2YQ1iIWm_bGYdY",
+    authDomain: "habitician.firebaseapp.com",
+    projectId: "habitician",
+    storageBucket: "habitician.appspot.com",
+    messagingSenderId: "754810832554",
+    appId: "1:754810832554:web:edc704336d8761a114b2d3",
+    measurementId: "G-QGZPQTWZE8",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
